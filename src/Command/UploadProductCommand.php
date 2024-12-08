e62d2940495e9a019c5282422a259ac1feae34f6
Javier Quiroga <quiroga218@gmail.com>
2024-12-07 20:00:23 -0600

    chore: config command to use csvimporter

--- src/Command/UploadProductCommand.php
+++ src/Command/UploadProductCommand.php
@@ -2,9 +2,9 @@
 
 namespace App\Command;
 
-use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Service\Importers\Importer;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
@@ -13,108 +13,59 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: 'app:upload-product',
-    description: 'Tool to upload product data in csv format'
+    description: 'Tool to upload product data in csv format',
+    hidden: false,
+    aliases: ['app:product-importer']
 )]
 class UploadProductCommand extends Command
 {
     private $entityManager;
+    private $importer;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(Importer $importer)
     {
-        $this->entityManager = $entityManager;
+        $this->importer = $importer;
         parent::__construct();
     }
 
     protected function configure(): void
     {
         $this
-        ->setDescription('Imports products from a CSV file into the database.')
-        ->addArgument('file', InputArgument::REQUIRED, 'Path to the CSV file to import');
+            ->addArgument('filePath', InputArgument::REQUIRED, 'Path to the CSV file')
+            ->addOption('test', null, InputOption::VALUE_NONE, 'Run in test mode (does not insert data into the database)');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $filePath = $input->getArgument('file');
+
+        $filePath = $input->getArgument('filePath');
+        $isTestMode = $input->getOption('test');
         $io = new SymfonyStyle($input, $output);
-        if (!file_exists($filePath)) {
-            $io->error("File not found: $filePath");
-            return Command::FAILURE;
-        }
 
-        $fileExtension = pathinfo($filePath, PATHINFO_EXTENSION);
-        if (strtolower($fileExtension) !== 'csv') {
-            $io->error('<error>Invalid file type. Only CSV files are allowed.</error>');
-            return Command::FAILURE;
-        }    
-        
-        $file = fopen($filePath, 'r');
-    
-        fgetcsv($file);
+        $result = $this->importer->file(Importer::CSV_IMPORTER)->import($filePath, $isTestMode);
 
-        //counters
-        $processed = 0;
-        $successful = 0;
-        $skipped = 0;
-        $notImported = [];
+        if ($isTestMode) {
+            $io->warning('Test mode enabled. No data was inserted into the database.');
+        }   
 
-        while (($data = fgetcsv($file)) !== false) {
-            $processed++;
-    
-            $stock = $data[3]??0;
+        $output->writeln([
+            'Importing CSV file...',
+            '==================',
+            '',
+        ]);
 
-            $price = $data[4]??0;
+        sleep(1);
 
-            if(is_numeric($price)){
-                $price = (float)$price;
-            }else {
-                $notImported[] = $data;
-                $skipped++;
-                continue;
-            }
+        $output->writeln([
+            "<info>Total Processed: {$result['processed']}</info>",
+            "<info>Successfully Imported: {$result['successful']}</info>"
+        ]);
 
-            if(is_numeric($stock)){
-                $stock = (int)$stock;
-            }else{
-                $notImported[] = $data;
-                $skipped++;
-                continue;
-            }
-            $isDiscontinued = strtolower( $data[5]?? '' ) === 'yes';
-    
-            if (($stock < 10 && $price < 5) || $price > 1000) {
-                $notImported[] = $data;
-                $skipped++;
-                continue;
-            }
-            try {
-            $product = new Product();
-            $product->setstrtProductName($data[1])
-                    ->setStrProductDesc($data[2])
-                    ->setStrProductCode($data[0])
-                    ->setStockLevel($stock)
-                    ->setPrice($price)
-                    ->setDtmDiscontinued($isDiscontinued ? new \DateTime() : null);
-                $this->entityManager->persist($product);
-                $this->entityManager->flush();
-                $successful++;
-            } catch (\Exception $e) {
-                $notImported[] = $data;
-                $skipped++;
-            }
-        }
-    
-        fclose($file);
-        
-        $io->info("Processed: $processed");
-        $io->text("Successful: $successful");
-        $io->text("Skipped: $skipped. See them down below in the following table:");
-        if($skipped > 0){
-            $io->table(
-                ["Product Code","Product Name",	"Product Description","Stock","Cost in GBP","Discontinued"
-            ],
-                $notImported
-            );
-
+        // Display rows that were skipped
+        if($result['errors']['rows'] > 0 ){
+            "<info></info>";
+            $io->error("Skipped: {$result['skipped']}. Please check the following:");
+            $io->table($result['errors']['header'], $result['errors']['rows']);
         }
         return Command::SUCCESS;
     }
